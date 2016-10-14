cfc49575b4401ca14b264300541d3f1eb58774cf
Dan Munro <dmunro@alegion.com>
2016-10-13 17:15:21 -0700

    use config file and add quit command

--- bootstrap.php
+++ bootstrap.php
@@ -6,28 +6,36 @@ require_once __DIR__.'/vendor/autoload.php';
 use PhpMud\ServiceProvider\Command\MoveCommand;
 use PhpMud\ServiceProvider\Command\LookCommand;
 use PhpMud\ServiceProvider\Command\NewRoomCommand;
+use PhpMud\ServiceProvider\Command\QuitCommand;
 
 use Pimple\Container;
+use Symfony\Component\Yaml\Yaml;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 
+$configFile = __DIR__.'/config.yaml';
+
+if (!file_exists($configFile)) {
+    die('need a config file');
+}
+
+$config = Yaml::parse(file_get_contents($configFile));
+
 $container = new Container();
 
-$container[EntityManager::class] = function () {
-    $isDevMode = true;
-    $config = Setup::createAnnotationMetadataConfiguration(
-        [
-            __DIR__.'/src/Entity'
-        ],
-        $isDevMode
-    );
+$container[EntityManager::class] = function () use ($config) {
 
     return EntityManager::create(
         [
             'driver' => 'pdo_sqlite',
             'path' => __DIR__ . '/db.sqlite'
         ],
-        $config
+        Setup::createAnnotationMetadataConfiguration(
+            [
+                __DIR__.'/src/Entity'
+            ],
+            $config['debug']
+        )
     );
 };
 
@@ -36,5 +44,6 @@ $commands = new Container();
 $commands->register(new MoveCommand());
 $commands->register(new LookCommand());
 $commands->register(new NewRoomCommand());
+$commands->register(new QuitCommand());
 
 $container['commands'] = $commands;
--- src/ServiceProvider/Command/QuitCommand.php
+++ src/ServiceProvider/Command/QuitCommand.php
@@ -3,16 +3,17 @@ declare(strict_types=1);
 
 namespace PhpMud\ServiceProvider\Command;
 
-use PhpMud\Command\Look;
+use PhpMud\Client;
+use PhpMud\Command\Quit;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 
-class LookCommand implements ServiceProviderInterface
+class QuitCommand implements ServiceProviderInterface
 {
     public function register(Container $pimple)
     {
-        $pimple['look'] = $pimple->protect(function () {
-            return new Look();
+        $pimple['quit'] = $pimple->protect(function (Client $client) {
+            return new Quit($client);
         });
     }
 }
