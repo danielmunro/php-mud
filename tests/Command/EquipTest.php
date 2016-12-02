64d9082c415190609bb8980d15f1a10ff7fd4f4b
Dan Munro <dan@danmunro.com>
2016-12-02 00:05:07 -0800

    test wear and remove

---
+++ tests/Command/EquipTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpMud\Tests\Command;
+
+use PhpMud\Entity\Item;
+use PhpMud\Enum\Material;
+use PhpMud\Enum\Position;
+
+class EquipTest extends CommandTest
+{
+    public function testEquip()
+    {
+        $client = $this->getMockClient();
+
+        $inv = $client->getMob()->getInventory();
+        $inv->add($this->getMockHelmet());
+        $inv->add($this->getMockHelmet());
+        $eq = $client->getMob()->getEquipped();
+        $commands = $this->getCommands();
+        static::assertCount(2, $inv->getItems());
+        static::assertCount(0, $eq->getItems());
+        $commands->execute($client->input('wear helmet'));
+        static::assertCount(1, $inv->getItems());
+        static::assertCount(1, $eq->getItems());
+        $commands->execute($client->input('remove helmet'));
+        static::assertCount(2, $inv->getItems());
+        static::assertCount(0, $eq->getItems());
+    }
+
+    private function getMockHelmet(): Item
+    {
+        $item = new Item('helmet', Material::COPPER(), ['helmet']);
+        $item->setPosition(Position::HEAD());
+
+        return $item;
+    }
+}
